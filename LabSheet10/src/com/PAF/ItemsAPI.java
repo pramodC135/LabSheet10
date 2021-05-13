ae4312807f3b47337aa5766502d9a60dedebe742
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-14 01:01:28 +0530

    Set up the DC-Bus

--- LabSheet10/src/com/PAF/ItemsAPI.java
+++ LabSheet10/src/com/PAF/ItemsAPI.java
@@ -6,12 +6,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
 
 /**
  * Servlet implementation class ItemsAPI
  */
 @WebServlet("/ItemsAPI")
 public class ItemsAPI extends HttpServlet {
+	Item itemObj = new Item();
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -35,7 +39,13 @@ public class ItemsAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String output = itemObj.insertItem(request.getParameter("itemCode"),
+							request.getParameter("itemName"),
+							request.getParameter("itemPrice"),
+							request.getParameter("itemDesc"));
+		response.getWriter().write(output);
 	}
 
 	/**
@@ -43,6 +53,15 @@ public class ItemsAPI extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		
+		String output = itemObj.updateItem(paras.get("hidItemIDSave").toString(), 
+							paras.get("itemCode").toString(), 
+							paras.get("itemName").toString(), 
+							paras.get("itemPrice").toString(), 
+							paras.get("itemDesc").toString()); 
+		
+		response.getWriter().write(output); 
 	}
 
 	/**
@@ -50,6 +69,33 @@ public class ItemsAPI extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Map paras = getParasMap(request); 
+		
+		String output = itemObj.deleteItem(paras.get("itemID").toString());
+		
+		response.getWriter().write(output); 
 	}
+	
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+		 Map<String, String> map = new HashMap<String, String>(); 
+		 try
+		 { 
+			 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+			 String queryString = scanner.hasNext() ? 
+					 			scanner.useDelimiter("\\A").next() : ""; 
+			 scanner.close(); 
+			 String[] params = queryString.split("&"); 
+			 for (String param : params) 
+			 {
+				 String[] p = param.split("=");
+				 map.put(p[0], p[1]); 
+			 } 
+		 } 
+		 catch (Exception e) 
+		 { 
+		 } 
+		 return map; 
+	 }
 
 }
